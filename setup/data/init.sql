46eb9214509071d000a7391bbdfb81cc487dbd81
jayrt <thapa.jayrt@gmail.com>
2020-07-31 03:45:58 +0000

    adding data to sql file

--- setup/data/init.sql
+++ setup/data/init.sql
@@ -20,11 +20,14 @@ CREATE TABLE blog_post (
         REFERENCES author (email)
 ) ENGINE = InnoDB;
 
-INSERT INTO author (email, first_name, last_name, password_hash) VALUES ("thapa.jayrt@gmail.com", "Jay", "Thapa", "123abc");
+INSERT INTO author (email, first_name, last_name, password_hash) VALUES ("thapa.jayrt@gmail.com", "Jay", "Thapa", "123abc"), 
+("zoldyckillua07@gmail.com", "Killua", "Zoldyck", "heaven@rena");
 
 INSERT INTO blog_post (slug, post_name, post_context, publication_date, author_email) VALUES
 ("BadPost", "Horrible Post", "OMG! what could be morse worse than this. How can someone be so horrible at something", NOW(), "thapa.jayrt@gmail.com"),
 
 ("NoClick", "Don't Click", "Trust my advice and make sure you stay away. Trust me you don't want to check the post", NOW(), "thapa.jayrt@gmail.com"),
 
-("Listening", "Are you even listening to me", "What you are even more excited to see what's in there. My gosh! Some people", NOW(), "thapa.jayrt@gmail.com");
+("Listening", "Are you even listening to me", "What you are even more excited to see what's in there. My gosh! Some people", NOW(), "zoldyckillua07@gmail.com"),
+
+("HadItComing", "Well U had it coming", "Decided to click anywasy huh. Well dont tell me I didn't warn you.", NOW(), "zoldyckillua07@gmail.com");
