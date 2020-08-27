aca9d915d0a37a1694dd5922d34ac72a9bb44004
jayrt <thapa.jayrt@gmail.com>
2020-08-27 04:48:53 +0000

    personal preference

--- setup/data/init.sql
+++ setup/data/init.sql
@@ -18,16 +18,16 @@ CREATE TABLE blog_post (
         REFERENCES author (email)
 ) ENGINE = InnoDB;
 
-INSERT INTO author (email, first_name, last_name, password_hash) VALUES ("naruto_dattebayo@konoha.com", "Naurto", "Uzumaki", "$2y$10$lYtbtyXc95weGeuUn6NGvuFcHGKSjONA6yaI.JOPuQWLDXU/9AvXO"), 
-("nagato_pain@akatsuki.com", "Nagato", "Uzumaki", "$2y$10$a2ylxsx6gQU1CUp7dTFMP.0xwRQqignVbOoaDhTWvretE02etBWIq");
--- passowrd for naruto = konohagakure
--- password for nagato = amegakure
+INSERT INTO author (email, first_name, last_name, password_hash) VALUES ("itachi_sharingan@konoha.com", "Itachi", "Uchiha", "$2y$10$c8oi3xhK2QJYrzVpTQQYguptd0oB1G31WJPlP5ZQPwN5QV6g3n0EK"), 
+("nagato_rinnegan@akatsuki.com", "Nagato", "Uzumaki", "$2y$10$gjVGsS15XPGhi2SvdNzk6erSfMi/x1R/dVhB310zIUYPxLWY3GTfS");
+-- passowrd for itachi = tsukuyomi
+-- password for nagato = shinratensei
 
 INSERT INTO blog_post (slug, post_name, post_context, publication_date, author_email) VALUES
-("badpost", "Horrible Post", "OMG! What could be morse worse than this? How can someone be so horrible at something?", NOW(), "naruto_dattebayo@konoha.com"),
+("badpost", "Horrible Post", "OMG! What could be more worse than this? How can someone be so horrible at something?", NOW(), "itachi_sharingan@konoha.com"),
 
-("donclick", "Don't Click", "Trust me when I say stay away from this. You do not want to check this post.", NOW(), "nagato_pain@akatsuki.com"),
+("donclick", "Don't Click", "Trust me when I say stay away from this. You do not want to check this post.", NOW(), "nagato_rinnegan@akatsuki.com"),
 
-("listening", "Are you even listening to me", "What! You are even more excited to see what's in there. My gosh! Some people", NOW(), "nagato_pain@akatsuki.com"),
+("listening", "Are you even listening to me", "What! You are even more excited to see what's in there. My gosh! Some people", NOW(), "nagato_rinnegan@akatsuki.com"),
 
-("uhaditcoming", "Well U had it coming", "Decided to click anyway, huh! Well, don't tell me I didn't warn you.", NOW(), "naruto_dattebayo@konoha.com");
+("uhaditcoming", "Well U had it coming", "Decided to click anyway, huh! Well, don't tell me I didn't warn you.", NOW(), "itachi_sharingan@konoha.com");
